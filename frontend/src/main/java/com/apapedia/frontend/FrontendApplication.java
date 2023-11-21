8c2b4251f8c19effda3524cf9524622efb240bbe
Fachry Anwar <fachryanwar2926@gmail.com>
2023-11-21 14:30:04 +0700

    style: update home page

--- frontend/src/main/java/com/apapedia/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/apapedia/frontend/FrontendApplication.java
@@ -3,7 +3,7 @@ package com.apapedia.frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication																																																																																																																																																																																																																																														
 public class FrontendApplication {
 	
 	public static void main(String[] args) {
