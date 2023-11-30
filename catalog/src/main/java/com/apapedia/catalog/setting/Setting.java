f4d3082f00fdd1202e18985ffa04d2bdec35fe51
Fachry Anwar Hamsyana <fachry.anwar@ui.ac.id>
2023-11-30 13:01:59 +0700

    fix: application properties

--- catalog/src/main/java/com/apapedia/catalog/setting/Setting.java
+++ catalog/src/main/java/com/apapedia/catalog/setting/Setting.java
@@ -3,17 +3,17 @@ package com.apapedia.catalog.setting;
 public class Setting {
     public static final String ORDER_SERVER_URL =
             System.getenv("ORDER_SERVER_URL") == null ?
-                    "http://127.0.0.1:8080/api/order" : System.getenv("ORDER_SERVER_URL");
+                    "http://apap-190.cs.ui.ac.id/api/order" : System.getenv("ORDER_SERVER_URL");
     public static final String CART_SERVER_URL =
             System.getenv("CART_SERVER_URL") == null ?
-                    "http://127.0.0.1:8080/api/cart" : System.getenv("CART_SERVER_URL");
+                    "http://apap-190.cs.ui.ac.id/api/cart" : System.getenv("CART_SERVER_URL");
     public static final String USER_SERVER_URL =
             System.getenv("USER_SERVER_URL") == null ?
-                    "http://127.0.0.1:8082/api" : System.getenv("USER_SERVER_URL");
+                    "http://apap-188.cs.ui.ac.id/api" : System.getenv("USER_SERVER_URL");
     public static final String CATALOG_SERVER_URL =
             System.getenv("CATALOG_SERVER_URL") == null ?
-                    "http://127.0.0.1:8081/api/catalog" : System.getenv("CATALOG_SERVER_URL");
+                    "http://apap-189.cs.ui.ac.id/api/catalog" : System.getenv("CATALOG_SERVER_URL");
     public static final String CATEGORY_SERVER_URL =
             System.getenv("CATEGORY_SERVER_URL") == null ?
-                    "http://127.0.0.1:8081/api/category" : System.getenv("CATEGORY_SERVER_URL");
+                    "http://apap-189.cs.ui.ac.id/api/category" : System.getenv("CATEGORY_SERVER_URL");
 }
