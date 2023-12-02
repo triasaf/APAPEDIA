03815cc20458bb852db88da84d4ba1781f4c2817
Fachry Anwar <fachryanwar2926@gmail.com>
2023-12-02 11:05:05 +0700

    fix: Setting.java using Dotenv

--- order/src/main/java/com/apapedia/order/setting/Setting.java
+++ order/src/main/java/com/apapedia/order/setting/Setting.java
@@ -1,19 +1,17 @@
 package com.apapedia.order.setting;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Setting {
-    public static final String ORDER_SERVER_URL =
-            System.getenv("ORDER_SERVER_URL") == null ?
-                    "http://apap-190.cs.ui.ac.id/api/order" : System.getenv("ORDER_SERVER_URL");
-    public static final String CART_SERVER_URL =
-            System.getenv("CART_SERVER_URL") == null ?
-                    "http://apap-190.cs.ui.ac.id/api/cart" : System.getenv("CART_SERVER_URL");
-    public static final String USER_SERVER_URL =
-            System.getenv("USER_SERVER_URL") == null ?
-                    "http://apap-188.cs.ui.ac.id/api" : System.getenv("USER_SERVER_URL");
-    public static final String CATALOG_SERVER_URL =
-            System.getenv("CATALOG_SERVER_URL") == null ?
-                    "http://apap-189.cs.ui.ac.id/api/catalog" : System.getenv("CATALOG_SERVER_URL");
-    public static final String CATEGORY_SERVER_URL =
-            System.getenv("CATEGORY_SERVER_URL") == null ?
-                    "http://apap-189.cs.ui.ac.id/api/category" : System.getenv("CATEGORY_SERVER_URL");
+    public static final String ORDER_SERVER_URL = Dotenv.load().get("ORDER_SERVER_URL",
+            "http://apap-190.cs.ui.ac.id/api/order");
+    public static final String CART_SERVER_URL = Dotenv.load().get("CART_SERVER_URL",
+            "http://apap-190.cs.ui.ac.id/api/cart");
+    public static final String USER_SERVER_URL = Dotenv.load().get("USER_SERVER_URL",
+            "http://apap-188.cs.ui.ac.id/api");
+    public static final String CATALOG_SERVER_URL = Dotenv.load().get("CATALOG_SERVER_URL",
+            "http://apap-189.cs.ui.ac.id/api/catalog");
+    public static final String CATEGORY_SERVER_URL = Dotenv.load().get("CATEGORY_SERVER_URL",
+            "http://apap-189.cs.ui.ac.id/api/category");
 }
+
